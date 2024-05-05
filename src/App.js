cab450951bfddb2915036b99cd3f5948249d2e00
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-05-05 13:01:31 +0530

    notification

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import Header from './views/Header';
 import { AllRoutes } from './views/Routes';
 import { ToastContainer } from 'react-toastify';
+import { generateToken, messaging } from './firebase';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { onMessage } from 'firebase/messaging';
 function App() {
+  useEffect(()=>{
+    generateToken();
+    onMessage(messaging,(payload)=>{
+      
+    })
+  },[])
   return (
     <div className="App">
       <Header></Header>
