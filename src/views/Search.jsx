57a2bd7be3bdf1890cbc4df667a8613c8451f7c1
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-05-15 12:51:07 +0530

    search

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef,useCallback } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toastSuccess, toastWarning } from './components/Notifications';
 import {useAuth} from './other/AuthContext'
@@ -13,15 +13,10 @@ export default function Search() {
     const [data, setData] = useState(JSON.parse(localStorage.getItem("torrent")) ||"");
     const [links, setLinks] = useState(JSON.parse(localStorage.getItem("torrentlinks")) ||[]);
     const [spinner,setSpinner]=useState(false);
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setQuery(query.current.value);
-        setSpinner(true);
-    }
    
     const fetchData = async () => {
         try {
-            const response = await fetch(`https://rsg-movies.vercel.app/api/search/?q=${queryData}&page=${page}`);
+            const response = await fetch(`https://rsg-movies.vercel.app/api/search/?q=${query.current.value}&page=${page}`);
             const result = await response.json();
             if(response.status==200){
 
@@ -44,12 +39,8 @@ export default function Search() {
     };
 
     useEffect(() => {
-       
-        localStorage.setItem("globalquery", JSON.stringify(queryData));
-        if (queryData) {
-            fetchData();
-        }
-        else {
+        if(query.current.value) fetchData()
+        else{
             setSpinner(false)
             setData("");
             setLinks([]);
@@ -57,8 +48,9 @@ export default function Search() {
             localStorage.removeItem("torrent");
             localStorage.removeItem("torrentlinks");
             localStorage.removeItem("torrentpages");
-        }
-    }, [queryData, page]);
+    }
+    }, [page]);
+   
     const addTorrent = async (link) => {
         startLoad();
         try {
@@ -87,12 +79,59 @@ export default function Search() {
         }
         stopLoad();
     }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setQuery(query.current.value);
+        localStorage.setItem("globalquery", JSON.stringify(query.current.value))
+        setSpinner(true);
+        if(query.current.value){
+          fetchData()
+          }
+          else {
+            setSpinner(false)
+            setData("");
+            setLinks([]);
+            setPages(null);
+            localStorage.removeItem("torrent");
+            localStorage.removeItem("torrentlinks");
+            localStorage.removeItem("torrentpages");
+        }
+      }
+    
+    const handleChange=()=>{
+        localStorage.setItem("globalquery", JSON.stringify(query.current.value))
+        setSpinner(true);
+        if(query.current.value){
+          fetchData()
+          }
+          else {
+            setSpinner(false)
+            setData("");
+            setLinks([]);
+            setPages(null);
+            localStorage.removeItem("torrent");
+            localStorage.removeItem("torrentlinks");
+            localStorage.removeItem("torrentpages");
+        } 
+      }
+    const debounce=(func)=>{
+      let timer;
+      return function(...args){
+        const context=this;
+        if(timer) clearTimeout(timer);
+        timer=setTimeout(()=>{
+          timer=null;
+          func.apply(context,args);
+        },300)
+      }
+    }
+    const optimized=useCallback(debounce(handleChange),[])
     return (
         <MyLoader>
         <main>
             <center>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-                <form className="max-w-md mx-auto">
+                <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
                     <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -100,8 +139,8 @@ export default function Search() {
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" id="default-search" value={queryData} ref={query} autoComplete="off" onChange={handleSubmit} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Anything here..." required />
-                        <button type="submit" onClick={handleSubmit} className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        <input type="search" id="default-search" value={queryData} ref={query} autoComplete="off" onChange={()=>{setQuery(query.current.value);optimized()}} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Anything here..."  />
+                        <button type="submit"  className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
                 <br />
--- src/views/home/Movierulz.jsx
+++ src/views/home/Movierulz.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect,useState,useRef } from 'react'
+import React, { useEffect,useState,useRef,useCallback } from 'react'
 import { Link } from 'react-router-dom';
 import { useAuth } from '../other/AuthContext';
 import {toastWarning} from '../components/Notifications'
 import MyLoader from '../MyLoader';
+import debounce from 'lodash.debounce';
 
 export default function Movierulz() {
   const {startLoad,stopLoad,loading}=useAuth();
@@ -10,11 +11,7 @@ export default function Movierulz() {
   const [spinner,setSpinner]=useState(false);
    const [queryData,setQuery]=useState(JSON.parse(localStorage.getItem("query")) ||"");
   const [movies,setMovies] = useState(JSON.parse(localStorage.getItem("movierulz")) || []);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setQuery(query.current.value);
-    setSpinner(true);
-  }
+ 
   const fetchData = async (apiUrl) => {
     startLoad();
     try {
@@ -35,17 +32,51 @@ export default function Movierulz() {
     setSpinner(false);
   };
   useEffect(() => {
-    localStorage.setItem("query", JSON.stringify(queryData));
     if(queryData){
     fetchData(`https://rsg-movies.vercel.app/api/movierulz/search/${queryData}`)
     }
     else{
       fetchData("https://rsg-movies.vercel.app/react/movierulz/");
-    }
-   
-    
-  }, [queryData]);
+    } 
+  }, []);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuery(query.current.value);
+    localStorage.setItem("query", JSON.stringify(query.current.value))
+    setSpinner(true);
+    if(query.current.value){
+      fetchData(`https://rsg-movies.vercel.app/api/movierulz/search/${query.current.value}`)
+      }
+      else{
+        fetchData("https://rsg-movies.vercel.app/react/movierulz/");
+      } 
+  }
+const handleChange=()=>{
+    localStorage.setItem("query", JSON.stringify(query.current.value))
+    setSpinner(true);
+    if(query.current.value){
+      fetchData(`https://rsg-movies.vercel.app/api/movierulz/search/${query.current.value}`)
+      }
+      else{
+        fetchData("https://rsg-movies.vercel.app/react/movierulz/");
+      } 
+  }
+const debounce=(func)=>{
+  let timer;
+  return function(...args){
+    const context=this;
+    if(timer) clearTimeout(timer);
+    timer=setTimeout(()=>{
+      timer=null;
+      func.apply(context,args);
+    },300)
+  }
+}
+const optimized=useCallback(debounce(handleChange),[])
   
+
+
+
   
   return (
     
@@ -53,7 +84,7 @@ export default function Movierulz() {
     <main >
       <center>
   
-      <form className="max-w-md mx-auto">   
+      <form className="max-w-md mx-auto" onSubmit={handleSubmit}>   
       <label  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -61,8 +92,8 @@ export default function Movierulz() {
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
           </div>
-          <input type="search" id="default-search" ref={query} value={queryData} autoComplete="off" onChange={handleSubmit}  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search movie here..." required />
-          <button type="submit" onClick={handleSubmit} className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          <input type="search" id="default-search" ref={query} value={queryData} autoComplete="off" onChange={()=>{setQuery(query.current.value);optimized()}}  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search movie here..."  />
+          <button type="submit"  className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
       </div>
   </form>
     
@@ -85,7 +116,7 @@ export default function Movierulz() {
     <Link to={`/movierulz/movie?link=${movie.link}`} >
         <img className="rounded-t-lg center" src={movie.image} alt="img" />
     <div className="p-1">
-      <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.name}</h5>
+      <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-3">{movie.name}</h5>
     </div>
     </Link>
 </div>
